99f33456f29725c5ec6059c7da7fff5c16359835
okanpiskin <31757732+okanpiskin@users.noreply.github.com>
2017-09-08 10:34:34 +0300

    DinamikDizilerEklendi

---
+++ DinamikDiziler/DinamikDiziler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Kitap Sayfa :147
+
+namespace DinamikDiziler //Dinamik dizilerde dizinin boyutu bir kez belirlendikten sonra artık değiştirilemez.Yani dizinin boyutunu dinamik
+                        //olarak değiştirmemiz mümkün değildir.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dinamik dizinin boyutunu giriniz...");
+            int val = Convert.ToInt32(Console.ReadLine());
+            int[] dinamikDizi = new int[val];
+
+            for (int i = 0; i < val; i++)
+            {
+                Console.WriteLine(dinamikDizi[i]);
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
